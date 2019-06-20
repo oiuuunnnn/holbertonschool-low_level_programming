1cb21c2210fe87c1eb7473e826df22a0ce51b270
oiuuunnnn <853@holbertonschool.com>
2019-06-20 22:56:18 +0000

    0

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: string
+*/
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
