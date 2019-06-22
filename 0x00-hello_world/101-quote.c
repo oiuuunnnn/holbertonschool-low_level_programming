0028a629c3e3b4121c9d14406eb2dd7c5ad30028
oiuuunnnn <853@holbertonschool.com>
2019-06-22 01:53:28 +0000

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: string
+*
+*/
+
+int main(void)
+{
+	printf(stderr, "and that piece of art is useful\" -\
+	 Dora Korpar, 2015-10-19\n");
+
+return (1);
+}
