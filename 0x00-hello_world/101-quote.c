a554335f21b3bad511310fb9a6125203a2e402a9
oiuuunnnn <853@holbertonschool.com>
2019-06-22 02:26:18 +0000

    rest

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,13 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" -\
-	 Dora Korpar, 2015-10-19\n");
+int rc = 48;
+
+if (rc)
+{
+fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+}
 
 return (1);
 }
