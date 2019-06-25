d10b26e2bb6fcad04ece594f77f3894e067c14d5
oiuuunnnn <853@holbertonschool.com>
2019-06-25 07:12:17 +0000

    l

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <_putchar.c>
+
+/**
+*main - entry point
+*
+*Return: string
+*/
+
+int main(void)
+{
+int st(9) = "Holberton";
+int ln;
+
+	for (int x = 0, x <= ln, x++)
+	ln = strlen(st);
+	_putchar(st);
+
+_putchar('\n');
+return (0);
+}
