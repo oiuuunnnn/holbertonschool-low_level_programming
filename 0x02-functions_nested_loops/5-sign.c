95d9cf52d85d87f3d799bd99e12f10c8727fcd91
oiuuunnnn <853@holbertonschool.com>
2019-06-25 19:29:24 +0000

    3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-*_print_sign - entry point
+*print_sign - entry point
 *
 *@n: character
 *
@@ -12,20 +12,20 @@ int print_sign(int n)
 
 {
 
-if (n > '0')
-{
-return (1);
-return ('+');
+if (n > 0)
+	{
+	_putchar('+');
+	return (1);
 }
-else if (n == '0')
+if (n < 0)
 {
-return (0);
-_putchar('0');
+	_putchar('-');
+	return (-1);
 }
-else if (n < '0')
+else
 {
-return (-1);
-_putchar('-');
+	_putchar('0');
+	return (0);
 }
 
 return (0);
