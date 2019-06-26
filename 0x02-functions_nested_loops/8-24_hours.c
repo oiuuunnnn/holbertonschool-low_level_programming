d930f93e6e91856625455f8515a4ed55c46236cb
oiuuunnnn <853@holbertonschool.com>
2019-06-26 23:52:41 +0000

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,24 +10,20 @@ void jack_bauer(void)
 
 {
 
-char h, m, hh, mm;
+char h, m;
 
-h = '0';
-m = '0';
-hh = (24 + 48);
-mm = (60 + 48);
+h = 0;
+m = 0;
 
-for (h = '0'; h <= hh; h++)
-	for (m = '0'; m <= mm; m++)
-	if (h < hh && m < mm)
+for (h = 0; h <= 24; h++)
+	for (m = 0; m <= 60; m++)
+	if (h < 24 && m < 60)
 	{
-	_putchar(h / 10);
-	_putchar(h % 10);
+	_putchar((h / 10) + '0');
+	_putchar((h % 10) + '0');
 	_putchar(':');
-	_putchar(m / 10);
-	_putchar(m % 10);
+	_putchar((m / 10) + '0');
+	_putchar((m % 10) + '0');
+	_putchar('\n');
 	}
-
-_putchar('\n');
-
 }
