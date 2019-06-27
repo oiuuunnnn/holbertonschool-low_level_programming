93b8ecb65b7a222edd121b2c6cc9b15f8788a43b
oiuuunnnn <853@holbertonschool.com>
2019-06-27 20:34:43 +0000

    l

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,6 +3,8 @@
 /**
 *print_square - entry point
 *
+*@size: var
+*
 *Return: 0
 */
 
@@ -10,4 +12,16 @@ void print_square(int size)
 
 {
 
+	int x;
+	int y;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (x = 1; x <= size; x++)
+			{
+			for (y = 1; y <= size; y++)
+			_putchar('#');
+			_putchar('\n');
+			}
 }
