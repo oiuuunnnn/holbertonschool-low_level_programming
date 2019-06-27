3ac477297acb261555333d0787a4e5fd4284ea72
oiuuunnnn <853@holbertonschool.com>
2019-06-27 22:56:01 +0000

    er

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,5 +9,21 @@
 void print_triangle(int size)
 
 {
+	int x, y, z;
 
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (x = 1; x <= size; x++)
+		{
+			z = size -x;
+			for (y = 1; y <= size; y++)
+			{
+				if (y <= z)
+				_putchar(' ');
+				else
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
 }
