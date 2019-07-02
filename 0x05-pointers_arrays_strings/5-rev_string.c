e02f266944ada4a9a906f5d9dc2107b7d3fa3066
oiuuunnnn <853@holbertonschool.com>
2019-07-02 21:44:52 +0000

    er

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,6 +10,23 @@
 void rev_string(char *s)
 {
 
+char rev[100] = "         ";
+int c = 0;
+int f = 0;
+int l;
 
+while (s[c] != '\0')
+	c++;
+
+l = c - 1;
+
+for (c = l; c >= 0; c--)
+	{
+	rev[f] = s[c];
+	f++;
+	}
+
+for (c = 0; c <= l; c++)
+	s[c] = rev[c];
 
 }
