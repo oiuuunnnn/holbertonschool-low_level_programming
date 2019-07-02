1d97047609a357459378a578870889fbdb2df9d6
oiuuunnnn <853@holbertonschool.com>
2019-07-02 18:17:58 +0000

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,13 +3,24 @@
 /**
 *print_rev - entry point
 *
-*@*n: var
+*@s: var
 *
 */
 
 void print_rev(char *s)
 {
 
+int c = 0;
 
+while (s[c] != '\0')
+	c++;
+
+while (c >= 0)
+	{
+	_putchar(s[c]);
+	c--;
+	}
+
+_putchar('\n');
 
 }
