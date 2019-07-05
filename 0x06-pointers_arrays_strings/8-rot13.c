26ac74e5792709e78780537ce3c3b4af80d4143c
oiuuunnnn <853@holbertonschool.com>
2019-07-05 22:14:43 +0000

    rot

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -14,14 +14,16 @@ char *rot13(char *s)
 int c;
 int ca;
 char a[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+char b[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
 
 for (c = 0; s[c] != '\0'; c++)
 	{
 	for (ca = 0; a[ca] != 'Z'; ca++)
-		if (s[c] == a[ca] && ca < 25)
-		s[c] = a[ca + 25];
-		else if (s[c] == a[ca] && ca > 25)
-		s[c] = a[(ca + 25) - 52];
+		if (s[c] == a[ca])
+			{
+			s[c] = b[ca];
+			break;
+			}
 	}
 
 return (s);
