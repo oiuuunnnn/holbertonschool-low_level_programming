3f12fbbb9026fcc9c74736f67124e303f7bfb8d4
oiuuunnnn <853@holbertonschool.com>
2019-07-06 02:03:24 +0000

    100

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -16,19 +16,33 @@ int x = n;
 int a = 1;
 int r;
 
-for (c = 0; x > 0; c++)
-	x = x / 10;
-
-for (x = 1; x < c; x++)
-	a = a * 10;
-
-for (x = 0; x < c; x++)
-	{
+if (n == 0)
+_putchar('0');
+else
+{
 	if (n < 0)
-	n = -n;
-	r = (n / a) % 10;
-	a = a / 10;
-	_putchar('0' + r);
-	}
+		x = -n;
 
+	for (c = 0; x > 0; c++)
+		x = x / 10;
+
+	for (x = 1; x < c; x++)
+		a = a * 10;
+
+	for (x = 0; x < c; x++)
+		{
+		if (n < 0)
+			{
+			r = ((-n / a) % 10) + '0';
+			a = a / 10;
+			_putchar(-r);
+			}
+		else
+			{
+			r = ((n / a) % 10) + '0';
+                        a = a / 10;
+                        _putchar(r);
+			}
+		}
+	}
 }
